329c3720080e65d566dee016443ec3219ef5fdce
Damilola <dammyoladipupo15@gmail.com>
2022-04-07 14:46:22 -0700

    Writing a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,2 @@
-#include <stdio.h>														     /**																  *main -entry point 														       *																    *Return: 0 after exectute														 */																      int main(void)															   {																		puts("\"Programming is like builfding a multilingual puzzle");									     return (0);														  }
+#include <stdio.h>
+/**																  *main -entry point 														       *																    *Return: 0 after exectute														 */																      int main(void)															   {																		puts("\"Programming is like builfding a multilingual puzzle");									     return (0);														  }
