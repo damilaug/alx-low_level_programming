61d7a4969a7294af6d22ef840ba437ecfe983034
Damilola <dammyoladipupo15@gmail.com>
2022-04-08 15:01:17 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char num;
+	
+for (num = 48; num <= 57; num++)
+{
+	putchar(num);
+}
+for (num = 97; num <= 102; num++)
+{
+	putchar(num);
+}
+putchar('\n');
+
+return (0);
+}
