fe075d8caf7cd1f7a5ea609136f2cd7371b8f79e
Damilola <dammyoladipupo15@gmail.com>
2022-04-08 12:56:30 -0700

    The last alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+	{
+		putchar(alpabet);
+	}
+	putchar ('\n');
+
+	return (0);
+}
