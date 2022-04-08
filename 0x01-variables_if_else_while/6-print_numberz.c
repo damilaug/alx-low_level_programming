f86b4ca0ca3e52ee0c5b69d7a653c813327b8220
Damilola <dammyoladipupo15@gmail.com>
2022-04-08 14:23:12 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+        int d;
+
+for (d = 48; d <= 57; d++)
+{
+        putchar (d);
+}
+putchar ("\n");
+
+return (0);
+}
