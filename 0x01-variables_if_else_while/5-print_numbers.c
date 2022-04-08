75fb82235db12e3731237162db6085675ccc67db
Damilola <dammyoladipupo15@gmail.com>
2022-04-08 14:02:06 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int d;
+
+for (d = 0; d <= 10; d++)
+{
+	printf("%d", d);
+}
+printf("\n");
+
+return (0);
+}
