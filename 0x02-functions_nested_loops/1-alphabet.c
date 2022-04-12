9458b286a56c8d23fbefb21f04f7a18bd3f34572
Damilola <dammyoladipupo15@gmail.com>
2022-04-12 13:59:29 -0700

    All lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * prints alphabet in lowercase
- *
+ * print_alphabet - Entry point
  * Return: always 0 Success
  */
 void print_alphabet(void)
@@ -11,4 +11,4 @@ void print_alphabet(void)
 	for (c = 'a'; c <= 'z'; c++)
 	_putchar(c);
 	_putchar('\n');
-}	
+}
