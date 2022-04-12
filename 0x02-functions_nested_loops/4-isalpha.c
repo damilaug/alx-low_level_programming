af35fc96df22a19cd66b5afe97aa57bf8096f38e
Damilola <dammyoladipupo15@gmail.com>
2022-04-12 15:44:46 -0700

    Lowercase and Uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,12 +6,5 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'Z') || (c >= 'a' && c <= 'z'))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 }
