c30742d5426d454869570a7b92ee6f357afbb8ee
Damilola <dammyoladipupo15@gmail.com>
2022-04-12 10:17:17 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * void: no argument
+ * Return: Always 0 (Sucess/correct)
+ */
+int main(void)
+{
+	char object[] = "_putchar";
+
+	int c;
+
+	for (c = 0 ; c < 8 ; c++)
+	{
+		_putchar(object[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is to practice and demonstrate the nested loops functions
