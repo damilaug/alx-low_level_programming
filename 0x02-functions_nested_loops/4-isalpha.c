cb573d39157e4f005da91f9c7342e137b77170a2
Damilola <dammyoladipupo15@gmail.com>
2022-04-12 15:46:58 -0700

    Lowercase and Uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: takes a character
  * Return: 1 if letter, lowercase, uppercase; 0 otherwise
  */
-int _isalpha(int c)
+int _isalpha (int c)
 {
 	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 }
