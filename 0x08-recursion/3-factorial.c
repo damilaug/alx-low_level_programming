8b21dd138b71254a473ce0a1427e05720a0fa6b2
Damilaug <dammyoladipupo15@gmail.com>
2022-04-28 07:06:38 -0700

    Factorial of a number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,15 +1,19 @@
 #include "main.h"
+
 /**
- * factorial - factorial of a given number
- * @n - pointer block of a memory to fill
- * Return - factorial
+ * factorial - main - check the code
+ * @n: number of factorial
+ * Return: Always 0.
  */
 int factorial(int n)
 {
 	if (n == 0)
+	{
 		return (1);
+	}
 	else if (n < 0)
+	{
 		return (-1);
-	else
-		return (n * factorial(n - 1));
+	}
+	return (n * factorial(n - 1));
 }
