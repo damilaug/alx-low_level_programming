adf51ca0809751efaa3021e71e7dd522b1149f60
Damilaug <dammyoladipupo15@gmail.com>
2022-05-16 12:57:33 -0700

    Print numbers given as parameters

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,34 +1,17 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * print_numbers - prints all numbers with a separator
- * @separator: separe numbers
- * @n: int, number of undefined arguments
  * print_numbers - prints numbers given as parameters
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list integer;
-unsigned int i;
 	unsigned int i;
 	va_list integer;
 
 	va_start(integer, n);
-	va_start(integer, n);
 
-for (i = 0; i < n; i++)
-{
-	printf("%i", va_arg(integer, int));
-	if (i != n - 1 && separator != NULL)
-		printf("%s", separator);
-}
-va_end(integer);
 	for (i = 0; i < n; i++)
 	{
 		if (!separator)
@@ -39,10 +22,7 @@ va_end(integer);
 			printf("%s%d", separator, va_arg(integer, int));
 	}
 
-putchar('\n');
-}
-va_end(integer);
+	va_end(integer);
 
 	printf("\n");
 }
-
